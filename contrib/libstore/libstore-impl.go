9e8e90ce057233d03821b0abd42758f0468ea1de
Adrian Trejo Nuñez <atrejo@andrew.cmu.edu>
2012-10-20 13:15:42 -0400

    Start libstore implementation
    I wrote the NewLibstore function. It just contacts the master server and
    fetches the list of storage servers (trying multiple times if they aren't
    ready).
    
    At this point, we should ignore all of the servers except the master when doing
    storage, then move on to routing requests once the basic code works.

--- contrib/libstore/libstore-impl.go
+++ contrib/libstore/libstore-impl.go
@@ -3,13 +3,45 @@ package libstore
 
 import (
 	"P2-f12/official/lsplog"
+	"P2-f12/official/storageproto"
+  "net/rpc"
+  "time"
 )
 
 type Libstore struct {
+  Nodes []storageproto.Node
+  RPCConn []*rpc.Client
 }
 
 func iNewLibstore(server, myhostport string, flags int) (*Libstore, error) {
-	return nil, lsplog.NotImplemented("iNewLibstore")
+  var store Libstore
+  var master *rpc.Client
+  var getargs storageproto.GetServersArgs
+  var getreply storageproto.RegisterReply
+  var err error
+
+  master, err = rpc.DialHTTP("tcp", server)
+  if lsplog.CheckReport(1, err) {
+	  return nil, lsplog.NotImplemented("iNewLibstore")
+  }
+
+  master.Call("GetServers", &getargs, &getreply)
+
+  for i := 0; (getreply.Ready == false) && (i < 5); i++ {
+    time.Sleep(1000 * time.Millisecond)
+    master.Call("GetServers", &getargs, &getreply)
+  }
+
+  // couldn't get list of servers from master
+  if getreply.Ready == false {
+	  return nil, lsplog.NotImplemented("iNewLibstore")
+  }
+
+  store.Nodes = getreply.Servers
+  store.RPCConn = make([]*rpc.Client, len(store.Nodes))
+  store.RPCConn[0] = master
+
+	return &store, nil
 }
 
 func (ls *Libstore) iGet(key string) (string, error) {
